454f2cd214434ca863bddd134768b261152bbe08
Jakob <mail@bauer-jakob.de>
2024-01-02 16:09:38 +0100

    Added docker compose to start the whole stack

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -2,9 +2,14 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind()],
     output: 'server',
     mode: 'production',
+    adapter: node({
+        mode: "standalone"
+    }),
 });

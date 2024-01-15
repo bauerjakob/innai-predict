a63b3690fa9853566c6a7e744aa2fa899b8f93e2
Jakob <mail@bauer-jakob.de>
2024-01-15 17:37:40 +0100

    Added sql init script

---
+++ docker/localDatabase/ai-models-init.sql
@@ -0,0 +1,48 @@
+INSERT INTO public."AiModels" (
+        "Id",
+        "ExternalId",
+        "Name",
+        "Default",
+        "PrecipitationMapSize",
+        "NumberOfInnLevels"
+    )
+VALUES (
+        1,
+        '897fe3db-b56e-42fa-be38-555810029dea',
+        'Zander Zenith',
+        false,
+        1024,
+        3
+    );
+INSERT INTO public."AiModels" (
+        "Id",
+        "ExternalId",
+        "Name",
+        "Default",
+        "PrecipitationMapSize",
+        "NumberOfInnLevels"
+    )
+VALUES (
+        2,
+        '60740723-5ce0-4989-9d79-ecf4c436a029',
+        'Salmon Swirl',
+        true,
+        64,
+        3
+    );
+INSERT INTO public."AiModels" (
+        "Id",
+        "ExternalId",
+        "Name",
+        "Default",
+        "PrecipitationMapSize",
+        "NumberOfInnLevels"
+    )
+VALUES (
+        3,
+        '043a0ff7-2f39-404a-a095-8710eff9106e',
+        'Roach River',
+        false,
+        256,
+        3
+    );
